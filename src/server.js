fc9470cae874f40ed755de2f69c476faa6b28457
Jordan Moss <jmoss636@gmail.com>
2018-04-17 00:41:07 +0100

    added basic stock chart from quandl

--- src/server.js
+++ src/server.js
@@ -2,14 +2,29 @@ const express = require('express')
 const PORT = process.env.PORT || 8080
 const API_KEY = process.env.API_KEY || require('./config.json').API_KEY
 const fetch = require('node-fetch')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
 const app = express()
 
-app.use(express.static(__dirname_ + '/public'))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'))
+
 console.log(API_KEY)
+
 app.get('/', (req, res) => {
-    fetch('https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=' + API_KEY).then(response =>response.json())
+   /* fetch('https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=' + API_KEY).then(response =>response.json())
+    .then(data => res.send(data))
+    .catch(err => res.send(err))*/
+    res.send('/')
+})
+
+app.post('/', (req, res) => {
+    const id = req.body.id
+    fetch(`https://www.quandl.com/api/v3/datasets/WIKI/${id}/data.json?api_key=${API_KEY}`).then(response =>response.json())
     .then(data => res.send(data))
     .catch(err => res.send(err))
+
 })
 
 
