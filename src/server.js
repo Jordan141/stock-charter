4a91e10004c6c57468d661acef33f9494c86db8b
Jordan Moss <jmoss636@gmail.com>
2018-04-16 19:37:16 +0100

    added index.html

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const API_KEY = process.env.API_KEY || require('./config.json').API_KEY
 const fetch = require('node-fetch')
 const app = express()
 
-
+app.use(express.static(__dirname_ + '/public'))
 console.log(API_KEY)
 app.get('/', (req, res) => {
     fetch('https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=' + API_KEY).then(response =>response.json())
