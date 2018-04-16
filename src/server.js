179260a6d552ab62c24b0a9b91450399c86f6661
Jordan Moss <jmoss636@gmail.com>
2018-04-16 19:34:25 +0100

    setup basic api

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const PORT = process.env.PORT || 8080
+const API_KEY = process.env.API_KEY || require('./config.json').API_KEY
+const fetch = require('node-fetch')
 const app = express()
 
 
-
+console.log(API_KEY)
 app.get('/', (req, res) => {
-    res.send('Hi!')
+    fetch('https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=' + API_KEY).then(response =>response.json())
+    .then(data => res.send(data))
+    .catch(err => res.send(err))
 })
 
 
