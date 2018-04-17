f9c9ba8c6c3cc9d7737576d7b7240ad87c0ece9c
Jordan Moss <jmoss636@gmail.com>
2018-04-17 18:31:18 +0100

    Setup ecosystem

--- src/server.js
+++ src/server.js
@@ -3,17 +3,18 @@ const PORT = process.env.PORT || 8080
 const API_KEY = process.env.API_KEY || require('./config.json').API_KEY
 const fetch = require('node-fetch')
 const bodyParser = require('body-parser')
+const cts = require('check-ticker-symbol')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io').listen(server)
-const ADD_STOCK = 'add_stock', REMOVE_STOCK = 'remove_stock', NEW_STOCK = 'new_stock'
+const ADD_STOCK = 'add_stock', REMOVE_STOCK = 'remove_stock', NEW_STOCK = 'new_stock', INVALID_STOCK = 'invalid_stock'
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'))
 
 console.log(API_KEY)
 
-let stocks = ['GE']
+let stocks = ['GE', 'AAPL']
 
 app.get('/', (req, res) => {
     res.send('/')
@@ -26,6 +27,9 @@ app.post('/', (req, res) => {
     .catch(err => res.send(err))
 
 })
+function validateSymbol(symbol){
+    return cts.valid(symbol)
+}
 
 io.on('connection', socket => {
     socket.on('join', function(data) {
@@ -34,11 +38,21 @@ io.on('connection', socket => {
     });
     socket.on(ADD_STOCK, function(data){
         console.log(ADD_STOCK, data)
-        socket.broadcast.emit(NEW_STOCK, stocks)
+        if(validateSymbol(data)){
+            stocks.push(data)
+            socket.broadcast.emit(NEW_STOCK, stocks)
+        } else {
+            socket.emit(INVALID_STOCK, data)
+        }
     })
     socket.on(REMOVE_STOCK, function(data){
         console.log(REMOVE_STOCK, data)
-        socket.broadcast.emit(NEW_STOCK, stocks)
+        if(validateSymbol(data) && stocks.includes(data)){
+            stocks.splice(stocks.indexOf(data), 1)
+            socket.broadcast.emit(NEW_STOCK, stocks)
+        } else {
+            socket.emit(INVALID_STOCK, data)
+        }
     })
 })
   
