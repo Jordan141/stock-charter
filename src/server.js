2d286e96e7244b66b9874d587eccd7635cbd0215
Jordan Moss <jmoss636@gmail.com>
2018-04-17 16:51:52 +0100

    basic websocket integration

--- src/server.js
+++ src/server.js
@@ -2,20 +2,20 @@ const express = require('express')
 const PORT = process.env.PORT || 8080
 const API_KEY = process.env.API_KEY || require('./config.json').API_KEY
 const fetch = require('node-fetch')
-const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server)
+const ADD_STOCK = 'add_stock', REMOVE_STOCK = 'remove_stock', NEW_STOCK = 'new_stock'
 
-app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'))
 
 console.log(API_KEY)
 
+let stocks = ['GE']
+
 app.get('/', (req, res) => {
-   /* fetch('https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=' + API_KEY).then(response =>response.json())
-    .then(data => res.send(data))
-    .catch(err => res.send(err))*/
     res.send('/')
 })
 
@@ -27,5 +27,21 @@ app.post('/', (req, res) => {
 
 })
 
+io.on('connection', socket => {
+    socket.on('join', function(data) {
+        console.log(data);
+        socket.emit(NEW_STOCK, stocks);
+    });
+    socket.on(ADD_STOCK, function(data){
+        console.log(data)
+        socket.broadcast.emit(NEW_STOCK, stocks)
+    })
+    socket.on(REMOVE_STOCK, function(data){
+        console.log(data)
+        socket.broadcast.emit(NEW_STOCK, stocks)
+    })
+})
+  
 
-app.listen(PORT)
+
+server.listen(PORT)
